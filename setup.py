e7c7cf168cbaac15d4555c475e680b55cf65dee5
Tanguy9862 <tanguy98.surowiec@gmail.com>
2023-10-08 04:14:10 +0200

    Prepared code for package distribution

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Next_Launch_Space_Scraper',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'bs4',
+        'requests',
+    ]
+)
