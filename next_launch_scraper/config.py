6934819f4d818fdf30c9ec8c7530245081f89464
Tanguy9862 <tanguy98.surowiec@gmail.com>
2025-01-11 01:39:38 +0100

    update the logic to get the .env file

--- next_launch_scraper/config.py
+++ next_launch_scraper/config.py
@@ -1,7 +1,8 @@
+from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '.env')
+env_path = Path.cwd() / ".env"
 load_dotenv(dotenv_path=env_path)
 
 
